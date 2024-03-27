91226029d16602b33371953557c0a2695b71e696
Pardis Pashakhanloo <ppashakhanloo@gmail.com>
2024-03-27 12:19:33 -0400

    separate blog pages

--- create_blog.py
+++ create_blog.py
@@ -1,22 +1,34 @@
-import sys, os
+import sys, os, re
+
+os.system("rm -rf blog && mkdir blog")
 
 with open('blog_layout.html', 'r') as f:
   layout = f.read()
 
 placeholder_text = '[THE_PLACEHOLDER_FOR_BLOG_LAYOUT]'
 
-blog_content = ""
+def get_title(content):
+    pattern = r'<title>(.*?)</title>'
+    return re.findall(pattern, content, re.DOTALL)[0]
+
+def get_body(content):
+    pattern = r'<post>(.*?)</post>'
+    return re.findall(pattern, content, re.DOTALL)[0]
+
+titles_list = ""
 for post in sorted(os.listdir('_posts'), reverse=True):
     if not post.startswith('post'):
         continue
     with open('_posts/'+post, 'r') as f:
         post_content = f.read()
+        title = get_title(post_content)
+        body = get_body(post_content)
         print(f"reading {post}")
+    with open(f'blog/{post}', 'w') as f:
+        f.write(layout.replace(placeholder_text, body))
 
-    blog_content += post_content + '\n\n'
-    print(len(blog_content))
+    titles_list += f"<p><a href=\"blog/{post}\">{title}</a></p>" + '\n\n'
 
-layout = layout.replace(placeholder_text, blog_content)
 with open('blog.html', 'w') as f:
-    f.write(layout)
+    f.write(layout.replace(placeholder_text, titles_list))
     f.write('\n')
